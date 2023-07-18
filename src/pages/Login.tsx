be95fed89032532b7f29e0d88d94d20e584301de
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-18 17:59:38 -0300

    feat: :boom: Adicionada validação de formulário no componente de login para garantir que os campos de email e senha sejam preenchidos corretamente antes de habilitar o botão de submit.
    Atualizei o componente de login para adicionar a validação do formulário. Agora, o botão de submit será habilitado somente quando tanto o campo de email quanto o campo de senha forem preenchidos corretamente. Além disso, incluí a verificação do formato do email e o comprimento mínimo da senha. A função validateForm é utilizada para realizar essas validações e atualizar o estado valid adequadamente. Dessa forma, o usuário receberá feedback imediato sobre os erros de entrada.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { userLogin } from '../redux/actions';
@@ -14,26 +14,64 @@ function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [form, setForm] = useState(initialState);
+  const [valid, setValid] = useState(false);
 
-  // Funções Handle
+  // Função de validação
+  const validateForm = () => {
+    const { email, password } = form;
+
+    if (!email || !password) {
+      setForm((prevForm) => (
+        { ...prevForm, error: 'Por favor, preencha todos os campos.' }
+      ));
+      return false;
+    }
+
+    if (!email.includes('@') || !email.includes('.com')) {
+      setForm((prevForm) => (
+        { ...prevForm, error: 'Por favor, insira um email válido.' }
+      ));
+      return false;
+    }
+
+    if (password.length < 6) {
+      setForm((prevForm) => (
+        { ...prevForm, error: 'A senha deve ter pelo menos 6 caracteres.' }
+      ));
+      return false;
+    }
+
+    setForm((prevForm) => ({ ...prevForm, error: '' }));
+    return true;
+  };
+
+  // Efeito para validar o formulário
+  useEffect(() => {
+    setValid(validateForm());
+  }, [form.email, form.password]);
+
   // Para o onChange
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setForm({ ...form, [name]: value });
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
   };
+
   // Para o onSubmit
   const handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
-    dispatch(userLogin(form));
-    navigate('/carteira');
+
+    if (valid) {
+      dispatch(userLogin(form));
+      navigate('/carteira');
+    }
   };
 
   return (
     <>
-      {/* Header - Cotendo o Titulo */}
-      <head>
+      {/* Header - Contendo o Titulo */}
+      <header>
         <h1>Login</h1>
-      </head>
+      </header>
 
       {/* Main - Contendo o Formulario */}
       <main>
@@ -44,6 +82,7 @@ function Login() {
             data-testid="email-input"
             type="email"
             name="email"
+            value={ form.email }
             onChange={ handleChange }
           />
 
@@ -53,13 +92,18 @@ function Login() {
             data-testid="password-input"
             type="password"
             name="password"
+            value={ form.password }
             onChange={ handleChange }
           />
+
+          {/* Mensagem de erro */}
+          {form.error && <p>{form.error}</p>}
 
           {/* Botão Submit */}
           <button
             type="submit"
             onClick={ handleSubmit }
+            disabled={ !valid }
           >
             Entrar
           </button>
