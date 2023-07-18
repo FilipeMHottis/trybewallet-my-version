a06d7b5fd63c9d1bdbbbf2f9e394d8343ff3889d
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-18 15:02:12 -0300

    feat: Comecei a fazer a pagina login, Ainda Precisa ser feito o redux e as rotas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,36 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      {/* Header - Cotendo o Titulo */}
+      <head>
+        <h1>Login</h1>
+      </head>
+
+      {/* Main - Contendo o Formulario */}
+      <main>
+        <form>
+          {/* Email */}
+          <label htmlFor="email">Email</label>
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+          />
+
+          {/* Password */}
+          <label htmlFor="password">Password</label>
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+          />
+
+          {/* Botão Submit */}
+          <button type="submit">Entrar</button>
+        </form>
+      </main>
+    </>
+  );
 }
 
 export default Login;
