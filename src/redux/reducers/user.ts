ba4b23467a7f91330ad1ea5d3d92650e107e4968
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-18 16:16:23 -0300

    feat: :construction: Adicionado e configurado o Redux com userReducer inicial e rootReducer.
    Adicionado o Redux ao projeto e configurado inicialmente. Implementado o userReducer, que é responsável por gerenciar o estado relacionado ao usuário. Neste momento, o reducer está em sua forma inicial e não possui nenhuma action implementada. Criado o rootReducer utilizando a função combineReducers, que permite combinar vários reducers em um único objeto de estado. Essas alterações são o ponto de partida para a implementação do gerenciamento de estado com Redux. O userReducer será responsável por tratar as ações relacionadas ao usuário, enquanto o rootReducer será utilizado para combinar os diferentes reducers que serão adicionados posteriormente.  A configuração do Redux abre caminho para a implementação de fluxos de dados e interações complexas entre componentes, facilitando o compartilhamento de estado de forma centralizada.

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { AnyAction } from 'redux';
+
+const initialState = {
+  email: '',
+  password: '',
+  loading: false,
+  error: '',
+};
+
+const userReducer = (state = initialState, action:AnyAction) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
+
+export default userReducer;
