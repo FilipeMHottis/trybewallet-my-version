64623faa7832cb6a35fceee74aced429ba5dd7a6
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-18 15:38:23 -0300

    feat: :truck: Add news Routes
    Adicionado novas rotas: "/" (que contém a página de login) e "/carteira" (que contém a página da carteira). Também adicionado um arquivo CSS base.
    
    Adicionadas novas rotas no aplicativo:  Rota "/" que contém a página de login. Rota "/carteira" que contém a página da carteira. Incluído um arquivo CSS baase para o projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+// Routes
+import { Route, Routes } from 'react-router-dom';
+
+// CSS BASE
+import './App.css';
+
+// Pages
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/carteira" element={ <Wallet /> } />
+    </Routes>
+  );
 }
 
 export default App;
