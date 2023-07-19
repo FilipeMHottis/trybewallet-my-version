5bd1b495b3ffc799f726ebea58ff3c9d2612a559
Filipe Marques Hottis <lipehottis@gmail.com>
2023-07-19 16:36:33 -0300

    fix: Adcionados os campos e botões

--- src/components/WalletForm.tsx
+++ src/components/WalletForm.tsx
@@ -38,12 +38,37 @@ function WalletForm() {
       {/* Campo para selecionar moeda */}
       <label htmlFor="currency-input">
         Moeda:
-        <select id="currency-input" data-testid="currency-input">
+        <select data-testid="currency-input">
           {wallet.currencies.map((currency) => (
             <option key={ currency } value={ currency }>{currency}</option>
           ))}
         </select>
       </label>
+
+      {/* Campo de Pagamento */}
+      <label htmlFor="payment-input">
+        Método de pagamento:
+        <select data-testid="method-input">
+          <option value="Dinheiro">Dinheiro</option>
+          <option value="Cartão de crédito">Cartão de crédito</option>
+          <option value="Cartão de débito">Cartão de débito</option>
+        </select>
+      </label>
+
+      {/* Campo de Categoria */}
+      <label htmlFor="category-input">
+        Categoria:
+        <select data-testid="tag-input">
+          <option value="Alimentação">Alimentação</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Transporte">Transporte</option>
+          <option value="Saúde">Saúde</option>
+        </select>
+      </label>
+
+      {/* Botão Submit */}
+      <button type="button">Adicionar despesa</button>
     </form>
   );
 }
